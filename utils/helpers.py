3cd98d5cd49cb8282f901dc3d212e18a338e5212
anushka-arunkumar <anushkaarun1999@gmail.com>
2025-11-26 08:35:23 +0530

    test git rm command

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,3 +7,12 @@ GRADE_RANGES = {
 }
 
 SUBJECTS = ["Math", "Science", "English", "History", "Geography"]
+
+
+# PASSING_PERCENTAGE = 50
+# PASSING_SUBJECT_SCORE = 40
+# TOTAL_SUBJECTS = 5
+# MAX_SCORE = 500  # Could be calculated as len(SUBJECTS) * 100
+
+# # Then use:
+# if student["percentage"] >= PASSING_PERCENTAGE and all(...)
